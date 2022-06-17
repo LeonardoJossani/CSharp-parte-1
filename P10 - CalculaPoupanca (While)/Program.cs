3df1a190417cfb271fb8a26a70c7c52d479a28a0
Leonardo Jossani <66314474+LeonardoJossani@users.noreply.github.com>
2022-06-17 17:32:20 -0300

    Atualização de arquivos de aula

---
+++ P10 - CalculaPoupanca (While)/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P10___CalculaPoupanca__While_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 10 - Calcula Poupança");
+
+            double valorInvestido = 1000;
+            int contadorMes = 1;
+
+            while (contadorMes <= 12)
+            {
+                // 3.36% = 0.0036
+                valorInvestido = valorInvestido + valorInvestido * 0.0036;
+                Console.WriteLine("Após " + contadorMes + " meses, você terá R$" + valorInvestido);
+
+                contadorMes++;
+            }
+                       
+            
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ P13 - ForEncadeado/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P13___ForEncadeado
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 13 - For Encadeado");
+
+            for (int contadorLinha = 0; contadorLinha < 10; contadorLinha++)
+            {
+                for (int contadorColuna = 0; contadorColuna < 10; contadorColuna++)
+                {
+                    Console.Write("*");
+                    if (contadorColuna >= contadorLinha)
+                        break;
+                }
+                Console.WriteLine();
+            }
+
+            // Uma outra forma de fazer o desenho de asteriscos
+            for (int contadorLinha = 0; contadorLinha < 10; contadorLinha++)
+            {
+                for (int contadorColuna = 0; contadorColuna <= contadorLinha; contadorColuna++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
